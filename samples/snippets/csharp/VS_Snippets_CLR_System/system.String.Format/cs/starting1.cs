1d8c11b066c713e1bb3dffb1a84b01089c9da048
Gernot Pokorny <gernot.pokorny.github@gmail.com>
2017-09-16 01:31:50 +0200

    Update starting1.cs
    The current inplementation of the "Controlling spacing"-example is bad practice.
     
    Have a look at this video, it explains why it is bad practice:
    https://www.youtube.com/watch?v=4lFAs6FYTXg
    
    Conclusion: In for-loops always use StringBuilder instead of string-concatenation. Strings are immutable and as a result you would create many unnecessary objects on the heap by using string-concatenation instead of StringBuilder.

--- samples/snippets/csharp/VS_Snippets_CLR_System/system.String.Format/cs/starting1.cs
+++ samples/snippets/csharp/VS_Snippets_CLR_System/system.String.Format/cs/starting1.cs
@@ -41,10 +41,11 @@ public class Example
        // <Snippet33>
        int[] years = { 2013, 2014, 2015 };
        int[] population = { 1025632, 1105967, 1148203 };
-       String s = String.Format("{0,6} {1,15}\n\n", "Year", "Population");
-       for(int index = 0; index < years.Length; index++)
-          s += String.Format("{0,6} {1,15:N0}\n",
-                             years[index], population[index]);
+       StringBuilder myStringBuilder = new StringBuilder();
+       for (int index = 0; index < years.Length; index++)
+       {
+          myStringBuilder.Append(String.Format("{0,6} {1,15:N0}\n", years[index], population[index]));
+       }
        // Result:
        //      Year      Population
        //
